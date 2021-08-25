bcc0c60304812c84f3d345c017d9ded3cd10e22b
Arvinder Palaha <arvinder.palaha@googlemail.com>
2021-08-25 09:29:45 +0100

    validated json data

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 import json
 from jsonschema import validate
+import chardet
+from codecs import BOM_UTF8
+import codecs
 
 def get_schema():
     """Load a schema"""
@@ -24,10 +27,23 @@ def detect_encoding(filename):
         encoding = chardet.detect(file.read())['encoding']
         return encoding
 
+def lstrip_bom(str_, bom=BOM_UTF8):
+    if str_.startswith(bom):
+        return str_[len(bom):]
+    else:
+        return str_
 
 print(detect_encoding('schema.json'))
 print(detect_encoding('compounds.json'))
 
+json_data = json.load(codecs.open('compounds.json', 'r', 'UTF-8-SIG'))
+print(json_data[0])
+print(codecs.BOM_UTF8)
+
+stripped_json_data = json.loads(lstrip_bom(open('compounds.json','rb').read()))
+# print(stripped_json_data[0])
+
+print(validate_json(stripped_json_data))
 
 msg = "Hello world"
 print(msg)
