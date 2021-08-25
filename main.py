c04ce5d602ad17b36c43cdc69bf393a149ce2430
Arvinder Palaha <arvinder.palaha@googlemail.com>
2021-08-25 13:48:11 +0100

    add dynamic legend to plot

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import json
 from jsonschema import validate
 from codecs import BOM_UTF8
+from matplotlib.markers import MarkerStyle
 
 import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
+import matplotlib.colors as mcolors
+from matplotlib import cm
 import numpy as np
 
 def lstrip_bom(str_, bom=BOM_UTF8):
@@ -44,15 +48,25 @@ for compound in json_data:
     ALogP.append(compound['ALogP'])
     num_rings.append(compound['num_rings'])
 
-# print(num_rings)
-area = [n*2 for n in num_rings]
-
-
+scale_num_rings = 10
+area = [scale_num_rings*n for n in num_rings]
+legend_cols = [scale_num_rings*n for n in set(num_rings)]
+legend_cols_norm = mcolors.Normalize(vmin=min(legend_cols), vmax=max(legend_cols))
+for n in legend_cols:
+    print(legend_cols_norm(n))
+viridis = cm.get_cmap('viridis', len(legend_cols))
 fig, ax = plt.subplots()
-ax.scatter(mol_weights, ALogP, s=area, c=num_rings)
-ax.set_title('molecular weight Vs ALogP')
+ax.scatter(mol_weights, ALogP, s=area, c=num_rings, alpha=0.5)
+ax.set_title('Molecular Weight vs ALogP')
 ax.set_xlabel('molecular weight')
 ax.set_ylabel('ALogP')
 ax.grid()
+legend_markers = []
+for n in legend_cols:
+    legend_markers.append(mlines.Line2D(
+        [], [], alpha=0.5, color=viridis(legend_cols_norm(n)),
+        marker='o', linestyle=None, linewidth=0, markersize=2*n/scale_num_rings,
+        label=str(n/scale_num_rings)))
+ax.legend(handles=legend_markers, title="number of rings")
 plt.show()
 
