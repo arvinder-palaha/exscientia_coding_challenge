b8e0a597a8bf00ceac5a0e7ffe64d787920c3a65
Arvinder Palaha <arvinder.palaha@googlemail.com>
2021-08-25 16:30:07 +0100

    building scatter function

--- analyser/plotting.py
+++ analyser/plotting.py
@@ -6,3 +6,25 @@ from matplotlib import cm
 
 def summ(a, b):
     return a + b
+
+def scatter(x, y, z=None, showplot=False, saveto=None):
+    """create scatter plot"""
+    assert len(x) == len(y)
+
+    scale_z = 10
+    
+    if z is not None:
+        d_color = z
+        d_area = [scale_z*zn for zn in z]
+    else:
+        d_color = [1 for x in x]
+        d_area = [1 for x in x]
+    
+    fig, ax = plt.subplots()
+    ax.scatter(x, y, s=d_area, c=d_color, alpha=0.5)
+    
+    if showplot:
+        plt.show()
+    if saveto is not None:
+        plt.savefig(saveto)
+    pass
