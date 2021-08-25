b70ecdfc69f9f5e960f0b03050bfe453d6f0f034
Arvinder Palaha <arvinder.palaha@googlemail.com>
2021-08-25 15:20:12 +0100

    test scatter x y equal

--- tests/test_plotting/test_plotting.py
+++ tests/test_plotting/test_plotting.py
@@ -1,4 +1,11 @@
-from analyser.plotting import summ
+import pytest
+from analyser.plotting import *
 
 def test_summ():
     assert summ(2,3) == 5
+
+def test_given_x_y_unequal_throw_exception():
+    with pytest.raises(Exception) as err:
+        x = [0, 1, 2, 3, 4, 5]
+        y = [0, 1, 2, 3]
+        scatter(x, y)
