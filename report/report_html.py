1d862b6390eb3eca8e0e0edb8abdfd873f0bc567
Arvinder Palaha <arvinder.palaha@googlemail.com>
2021-08-26 19:19:53 +0100

    populate table

--- report/report_html.py
+++ report/report_html.py
@@ -14,8 +14,18 @@ class table_html:
             s_name = str(property)
             s_type = str(prop_dict['type'])
             self.headers.append([s_name, s_type])
-        self.table.field_names = [name for name in self.headers]
-    
+        self.table.field_names = [name[0] for name in self.headers]
+
+    def populate(self, data_list):
+        for data in data_list:
+            row_to_add = []
+            for head in self.headers:
+                if head[1] != 'array':
+                    row_to_add.append(data[head[0]])
+                else:
+                    row_to_add.append("link")
+            self.table.add_row(row_to_add)
+
     def get_string(self):
         return self.table.get_string()
     
