9ba7fe188e39bdb4b0ffe224fd2c7f3db8598605
Arvinder Palaha <arvinder.palaha@googlemail.com>
2021-08-26 18:19:43 +0100

    added fixture to report testing

--- tests/test_report/test_report_html.py
+++ tests/test_report/test_report_html.py
@@ -3,10 +3,13 @@ import json
 from analyser.data_importer import _get_json_data_from_file
 from report.report_html import report_html
 
-def test_get_headers_from_schema_json():
-    schema = _get_json_data_from_file('tests/test_report/schema.json')
+@pytest.fixture
+def test_schema():
+    return _get_json_data_from_file('tests/test_report/schema.json')
+
+def test_get_headers_from_schema_json(test_schema):
     test_table = report_html()
-    test_table._column_headers(schema)
+    test_table._column_headers(test_schema)
     table_headers = test_table.headers
     expected_headers = [['compound_id', 'integer'],
         ['smiles', 'string'],
@@ -18,5 +21,7 @@ def test_get_headers_from_schema_json():
         ['assay_results', 'array'],
     ]
     assert table_headers == expected_headers
+
+# def test_create_empty_table_with_headers():
 
 
