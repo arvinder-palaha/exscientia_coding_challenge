a88d48a3c4807df2e1827f91a4a9f2069bbb6591
Arvinder Palaha <arvinder.palaha@googlemail.com>
2021-08-26 23:40:13 +0100

    argparse in main

--- main.py
+++ main.py
@@ -1,41 +1,46 @@
-from matplotlib.markers import MarkerStyle
-import matplotlib.pyplot as plt
-import matplotlib.lines as mlines
-import matplotlib.colors as mcolors
-from matplotlib import cm
+from analyser.data_importer import import_json, _get_json_data_from_file
+from analyser.plotting import scatter_plot
+from report.report_html import *
+import argparse
+import os
+import sys
 
-from analyser.data_importer import import_json
+parser = argparse.ArgumentParser()
+parser.add_argument('json_file', type=str)
+parser.add_argument('schema_file', type=str)
+parser.add_argument('--plot', action="store_true", help='plot molecular weight vs ALogP')
+parser.add_argument('--report', action="store_true", help='create report of compounds results as build/index.html')
+arguments = parser.parse_args()
 
-json_data = import_json(json_file='data/compounds.json',schema_file='data/schema.json')
+if not os.path.exists(arguments.json_file):
+    print('no such file:', arguments.json_file)
+    sys.exit()
 
-mol_weights = []
-ALogP = []
-num_rings = []
+if not os.path.exists(arguments.schema_file):
+    print('no such file:', arguments.schema_file)
+    sys.exit()
 
-for compound in json_data:
-    mol_weights.append(compound['molecular_weight'])
-    ALogP.append(compound['ALogP'])
-    num_rings.append(compound['num_rings'])
+json_data = import_json(json_file=arguments.json_file,schema_file=arguments.schema_file)
+print("json data validated against schema")
 
-scale_num_rings = 10
-area = [scale_num_rings*n for n in num_rings]
-legend_cols = [scale_num_rings*n for n in set(num_rings)]
-legend_cols_norm = mcolors.Normalize(vmin=min(legend_cols), vmax=max(legend_cols))
-for n in legend_cols:
-    print(legend_cols_norm(n))
-viridis = cm.get_cmap('viridis', len(legend_cols))
-fig, ax = plt.subplots()
-ax.scatter(mol_weights, ALogP, s=area, c=num_rings, alpha=0.5)
-ax.set_title('Molecular Weight vs ALogP')
-ax.set_xlabel('molecular weight')
-ax.set_ylabel('ALogP')
-ax.grid()
-legend_markers = []
-for n in legend_cols:
-    legend_markers.append(mlines.Line2D(
-        [], [], alpha=0.5, color=viridis(legend_cols_norm(n)),
-        marker='o', linestyle=None, linewidth=0, markersize=2*n/scale_num_rings,
-        label=str(n/scale_num_rings)))
-ax.legend(handles=legend_markers, title="number of rings")
-plt.show()
+if arguments.plot:
+    mol_weights = []
+    ALogP = []
+    num_rings = []
+
+    for compound in json_data:
+        mol_weights.append(compound['molecular_weight'])
+        ALogP.append(compound['ALogP'])
+        num_rings.append(compound['num_rings'])
+
+    scatter_plot(mol_weights, ALogP, num_rings, showplot=True, 
+        title="Molecular Weight vs ALogP",
+        xlabel='molecular weight',
+        ylabel='ALogP',
+        legend_title='number of rings')
+
+if arguments.report:
+    schema = _get_json_data_from_file(arguments.schema_file)
+    generate_html_report(json_data, schema, image_dir='data/images')
+    print('html report generated')
 
