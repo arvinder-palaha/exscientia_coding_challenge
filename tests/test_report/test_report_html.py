6f908475beb9c92c1ea0c69894a18bb76415b054
Arvinder Palaha <arvinder.palaha@googlemail.com>
2021-08-26 19:19:46 +0100

    refactored testing due to smaller data added test for filled table

--- tests/test_report/test_report_html.py
+++ tests/test_report/test_report_html.py
@@ -8,23 +8,32 @@ def test_schema():
     return _get_json_data_from_file('tests/test_report/schema.json')
 
 @pytest.fixture
+def test_compounds():
+    return _get_json_data_from_file('tests/test_report/test_compound.json')
+
+@pytest.fixture
 def exp_main_headers():
     return [['compound_id', 'integer'],
         ['smiles', 'string'],
-        ['molecular_weight', 'number'],
-        ['ALogP', 'number'],
-        ['molecular_formula', 'string'],
-        ['num_rings', 'number'],
-        ['image', 'string'],
         ['assay_results', 'array'],
     ]
 
 @pytest.fixture
 def exp_empty_table_str():
-    string = "+----------------------------+----------------------+--------------------------------+---------------------+---------------------------------+-------------------------+---------------------+----------------------------+\n"
-    string += "| ['compound_id', 'integer'] | ['smiles', 'string'] | ['molecular_weight', 'number'] | ['ALogP', 'number'] | ['molecular_formula', 'string'] | ['num_rings', 'number'] | ['image', 'string'] | ['assay_results', 'array'] |\n"
-    string += "+----------------------------+----------------------+--------------------------------+---------------------+---------------------------------+-------------------------+---------------------+----------------------------+\n"
-    string += "+----------------------------+----------------------+--------------------------------+---------------------+---------------------------------+-------------------------+---------------------+----------------------------+"
+    string =  "+-------------+--------+---------------+\n"
+    string += "| compound_id | smiles | assay_results |\n"
+    string += "+-------------+--------+---------------+\n"
+    string += "+-------------+--------+---------------+"
+    return string
+
+@pytest.fixture
+def exp_filled_table():
+    string =  "+-------------+----------------------------------------------------------+---------------+\n"
+    string += "| compound_id |                          smiles                          | assay_results |\n"
+    string += "+-------------+----------------------------------------------------------+---------------+\n"
+    string += "|   1117973   |  Cc1nnc2[C@H](NC(=O)OCc3ccccc3)N=C(c4ccccc4)c5ccccc5n12  |      link     |\n"
+    string += "|    694811   | CCNC(=O)C[C@@H]1N=C(c2ccc(Cl)cc2)c3cc(OC)ccc3n4c(C)nnc14 |      link     |\n"
+    string += "+-------------+----------------------------------------------------------+---------------+"
     return string
 
 def test_get_headers_from_schema_json(test_schema, exp_main_headers):
@@ -40,5 +49,11 @@ def test_use_schema_as_argument(test_schema, exp_main_headers):
 def test_create_empty_table_with_headers(test_schema, exp_empty_table_str):
     test_table = table_html(test_schema)
     table_str = test_table.get_string()
-    print(len(table_str), table_str)
     assert table_str == exp_empty_table_str
+
+def test_create_filled_headed_table(test_schema, test_compounds, exp_filled_table):
+    test_table = table_html(test_schema)
+    test_table.populate(test_compounds)
+    print(test_table.get_string())
+    assert test_table.get_string() == exp_filled_table
+
