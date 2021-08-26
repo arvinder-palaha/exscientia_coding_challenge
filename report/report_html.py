99264217c4253f2f69fc4a728e66cb49a8e07d4f
Arvinder Palaha <arvinder.palaha@googlemail.com>
2021-08-26 17:59:36 +0100

    function for column headers

--- report/report_html.py
+++ report/report_html.py
@@ -1,7 +1,15 @@
 from prettytable import PrettyTable
 
-def _headers(schema):
-    pass
+def _column_headers(schema):
+    headers = []
+    properties = schema['properties']
+    for property in properties:
+        prop_dict = properties[property]
+        s_name = str(property)
+        s_type = str(prop_dict['type'])
+        headers.append([s_name, s_type])
+    return headers
+
 
 # god function
 def generate_html_report():
--- tests/test_report/test_report_html.py
+++ tests/test_report/test_report_html.py
@@ -1,11 +1,11 @@
 import pytest
 import json
 from analyser.data_importer import _get_json_data_from_file
-from report.report_html import _headers
+from report.report_html import _column_headers
 
 def test_get_headers_from_schema_json():
     schema = _get_json_data_from_file('tests/test_report/schema.json')
-    table_headers = _headers(schema)
+    table_headers = _column_headers(schema)
     expected_headers = [['compound_id', 'integer'],
         ['smiles', 'string'],
         ['molecular_weight', 'number'],
