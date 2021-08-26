44d9ee5f349d2dce88c89c2eb8eb9dc8168cae5d
Arvinder Palaha <arvinder.palaha@googlemail.com>
2021-08-26 18:28:34 +0100

    expected headers as ficture

--- tests/test_report/test_report_html.py
+++ tests/test_report/test_report_html.py
@@ -7,11 +7,9 @@ from report.report_html import table_html
 def test_schema():
     return _get_json_data_from_file('tests/test_report/schema.json')
 
-def test_get_headers_from_schema_json(test_schema):
-    test_table = table_html()
-    test_table._column_headers(test_schema)
-    table_headers = test_table.headers
-    expected_headers = [['compound_id', 'integer'],
+@pytest.fixture
+def exp_main_headers():
+    return [['compound_id', 'integer'],
         ['smiles', 'string'],
         ['molecular_weight', 'number'],
         ['ALogP', 'number'],
@@ -20,9 +18,16 @@ def test_get_headers_from_schema_json(test_schema):
         ['image', 'string'],
         ['assay_results', 'array'],
     ]
-    assert table_headers == expected_headers
 
-# def test_use_schema_as_argument
+def test_get_headers_from_schema_json(test_schema, exp_main_headers):
+    test_table = table_html()
+    test_table._column_headers(test_schema)
+    table_headers = test_table.headers
+    assert table_headers == exp_main_headers
+
+# def test_use_schema_as_argument(test_schema):
+#     test_table = table_html(test_schema)
+#     assert True
 
 # def test_create_empty_table_with_headers(test_schema):
 #     test_table = report_html()
