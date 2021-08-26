dd5995294912fef7e793c73c9adab6741e784cd9
Arvinder Palaha <arvinder.palaha@googlemail.com>
2021-08-26 18:41:24 +0100

    test for headed table that is empty

--- tests/test_report/test_report_html.py
+++ tests/test_report/test_report_html.py
@@ -19,6 +19,14 @@ def exp_main_headers():
         ['assay_results', 'array'],
     ]
 
+@pytest.fixture
+def exp_empty_table_str():
+    string = "+----------------------------+----------------------+--------------------------------+---------------------+---------------------------------+-------------------------+---------------------+----------------------------+\n"
+    string += "| ['compound_id', 'integer'] | ['smiles', 'string'] | ['molecular_weight', 'number'] | ['ALogP', 'number'] | ['molecular_formula', 'string'] | ['num_rings', 'number'] | ['image', 'string'] | ['assay_results', 'array'] |\n"
+    string += "+----------------------------+----------------------+--------------------------------+---------------------+---------------------------------+-------------------------+---------------------+----------------------------+\n"
+    string += "+----------------------------+----------------------+--------------------------------+---------------------+---------------------------------+-------------------------+---------------------+----------------------------+"
+    return string
+
 def test_get_headers_from_schema_json(test_schema, exp_main_headers):
     test_table = table_html()
     test_table._column_headers(test_schema)
@@ -28,11 +36,9 @@ def test_get_headers_from_schema_json(test_schema, exp_main_headers):
 def test_use_schema_as_argument(test_schema, exp_main_headers):
     test_table = table_html(test_schema)
     assert test_table.headers == exp_main_headers
-    
 
-# def test_create_empty_table_with_headers(test_schema):
-#     test_table = report_html()
-#     test_table._column_headers(test_schema)
-    
-
-
+def test_create_empty_table_with_headers(test_schema, exp_empty_table_str):
+    test_table = table_html(test_schema)
+    table_str = test_table.get_string()
+    print(len(table_str), table_str)
+    assert table_str == exp_empty_table_str
