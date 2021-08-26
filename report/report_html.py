922e69043c0646c64614470e5e86a4ba5ff68ae4
Arvinder Palaha <arvinder.palaha@googlemail.com>
2021-08-26 17:11:43 +0100

    testing for html report methods

---
+++ report/report_html.py
@@ -0,0 +1,10 @@
+from prettytable import PrettyTable
+
+def _headers(schema):
+    pass
+
+# god function
+def generate_html_report():
+    """Created a tabulated report of the json data file
+    contents as a html file"""
+    pass
---
+++ tests/test_report/test_report_html.py
@@ -0,0 +1,18 @@
+import pytest
+import json
+from analyser.data_importer import _get_json_data_from_file
+from report.report_html import _headers
+
+def test_get_headers_from_schema_json():
+    schema = _get_json_data_from_file('tests/test_report/schema.json')
+    table_headers = _headers(schema)
+    expected_headers = [['compound_id', 'integer'],
+        ['smiles', 'string'],
+        ['molecular_weight', 'number'],
+        ['ALogP', 'number'],
+        ['molecular_formula', 'string'],
+        ['num_rings', 'number'],
+        ['image', 'string'],
+        ['assay_results', 'array'],
+    ]
+    assert table_headers == expected_headers
